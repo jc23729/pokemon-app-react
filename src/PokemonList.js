390edcbfc4bb12a6ac3313bc0ce8288d64cd44bd
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-04 17:54:29 -0400

    Update PokemonList.js

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
-export default function PokemonList() {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from "react";
+//so here we are destructring pokemon
+export default function PokemonList({ pokemon }) {
+  return <div></div>;
 }
