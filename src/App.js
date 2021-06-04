5da38c5d7ce1290249ff033eb71f95daa6856d18
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-04 17:46:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-   null
-  );
+  //first variable is our list of pokemon using on screen, current state, current data
+  //method we can use to update our state
+  // so basically it puts "bulbasaur", "charmander" into our pokemon variable on the left
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  return null;
 }
 
 export default App;
