aee103d501a1a09b158b951a6bef916b9ef9c21e
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-05 11:56:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   //first variable is our list of pokemon using on screen, current state, current data
   //method we can use to update our state
   // so basically it puts "bulbasaur", "charmander" into our pokemon variable on the left
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
   return (
     <PokemonList> pokemon={pokemon}</PokemonList>
   );
