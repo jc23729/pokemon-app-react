e55ac81014769c2e5fb2ab721e0d20251400a6fb
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-05 20:58:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import axios from "axios";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+//So we want to wrap useEffect around the request
+  useEffect(() => {
+      axios.get(" https://pokeapi.co/api/v2/pokemon").then((res) => {
+        //this will be our actual results for the pokemon
+        setPokemon(res.data.results.map((p) => p.name));
+      }, []);
 
   //were gonna make usestate dynamic using library api axios
 
-  axios.get(" https://pokeapi.co/api/v2/pokemon").then((res) => {
-    //this will be our actual results for the pokemon
-    setPokemon(res.data.results.map((p) => p.name));
   });
 
   return <PokemonList pokemon={pokemon} />;
